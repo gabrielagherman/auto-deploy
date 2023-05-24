beb8d3d59877768a9cf833a5355305899470abe3
gabrielagherman <126785247+gabrielagherman@users.noreply.github.com>
2023-05-24 22:56:20 +0300

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,59 +2,11 @@ provider "aws" {
   region     = var.region
   }
 
-
- #Security group
-resource "aws_security_group" "sg" {
-  name        = "allow_ssh_http"
-  description = "Allow ssh http inbound traffic"
-  vpc_id      = aws_vpc.app_vpc.id
-
-  ingress {
-    description      = "SSH from VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
- ingress {
-    description      = "Tomcat Server"
-    from_port        = 8080
-    to_port          = 8080
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  ingress {
-    description      = "HTTP from VPC"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "allow_ssh_http"
-  }
-}
-
 #create EC2 instance
 resource "aws_instance" "web" {
   ami             = "ami-04e601abe3e1a910f" 
   instance_type   = var.instance_type
   key_name        = var.instance_key
-  security_groups = [aws_security_group.sg.id]
 
   tags = {
     Name = "App-target"
